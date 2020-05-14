4d5c7e8e17c8baf24734e03ab3c4967ec457ca69
Hugo <hnlopez89@gmail.com>
2020-05-14 22:32:11 +0200

    corrección entrega 1 ejercicio dado

--- js/entrega-1/www/js/ejercicio3.js
+++ js/entrega-1/www/js/ejercicio3.js
@@ -1,14 +1,17 @@
 "use strict";
 
 let puntuacionTotal = 0;
-let dice = Math.ceil(Math.random() * 6);
 
 for (let i = 1; puntuacionTotal < 50; i++) {
+  let dice = Math.ceil(Math.random() * 6);
   puntuacionTotal += dice;
-}
-
-if (puntuacionTotal < 51) {
-  console.log(`${i}, dice, puntuacionTotal`);
-} else {
-  console.log(`${i}, dice, puntuacionTotal`);
+  if (puntuacionTotal < 50) {
+    console.log(
+      `Tirada: ${i}: ha salido un ${dice}, Tienes un total de ${puntuacionTotal}`
+    );
+  } else {
+    console.log(
+      `¡Enhorabuena, ha salido un ${dice}! ¡Has ganado con un total de ${puntuacionTotal} puntos`
+    );
+  }
 }
