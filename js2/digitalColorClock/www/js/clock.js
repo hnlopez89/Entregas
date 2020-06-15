2ea8658d1e60edb5d1e8472fdfab5ac16172cc17
Hugo <hnlopez89@gmail.com>
2020-06-15 14:38:09 +0200

    update digitalClock

--- js2/digitalColorClock/www/js/clock.js
+++ js2/digitalColorClock/www/js/clock.js
@@ -2,6 +2,10 @@ const body = document.querySelector("body");
 const main = body.children[2];
 const h1 = main.firstElementChild;
 const h2 = main.lastElementChild;
+const now = new Date();
+const hours = format(now.getHours());
+const minutes = format(now.getMinutes());
+const seconds = format(now.getSeconds());
 
 function format(number) {
   if (number >= 10) {
@@ -12,24 +16,18 @@ function format(number) {
 }
 
 function writeClock() {
-  const now = new Date();
-  const hours = format(now.getHours());
-  const minutes = format(now.getMinutes());
-  const seconds = format(now.getSeconds());
   h1.textContent = `${hours}:${minutes}:${seconds}`;
 }
+
 function greet() {
-  if (hours < 7 || hours > 22) {
+  if (`${hours}` < 7 || `${hours}` > 22) {
     return (h2.textContent = "Buenas Noches");
-  } else if (hours > 14) {
+  } else if (`${hours}` > 14) {
     return (h2.textContent = "Buenas Tardes");
   } else {
     return (h2.textContent = "Buenos días");
   }
 }
-
-setInterval(writeClock, 1000);
-
 function bodyColor() {
   if (h2.textContent === "Buenas Tardes") {
     body.setAttribute("class", "blue");
@@ -39,5 +37,7 @@ function bodyColor() {
     body.setAttribute("class", "dark");
   }
 }
+setInterval(writeClock, 100);
 
 bodyColor();
+greet();
